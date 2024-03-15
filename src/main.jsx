d94d1b57d0f72445df54636eb5c85e9faa0aea51
Arya <arya12345kishan@gmail.com>
2024-03-15 11:57:28 +0530

    Link_Change

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios"
 
-// axios.defaults.baseURL = ("https://my-mern-ecommerce.vercel.app")
-axios.defaults.baseURL = ("http://localhost:8080")
+axios.defaults.baseURL = ("https://my-mern-ecommerce.vercel.app")
+// axios.defaults.baseURL = ("http://localhost:8080")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
